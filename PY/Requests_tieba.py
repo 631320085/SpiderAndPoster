d57db0cde61e7cbaf7a7097f37a4a2bd2a4249ae
CodeFarmer <631320085@163.com>
2016-10-18 13:08:22 +0800

    tieba改为配置文件配置postdata

--- PY/Requests_tieba.py
+++ PY/Requests_tieba.py
@@ -3,17 +3,39 @@ import os
 import re
 import time
 import urllib.parse
+import random
+from bs4 import BeautifulSoup
+
+
+def PostContent(s, postUrl, reply, postData):
+    success = False
+    while not success:
+        for j in range(1, 4):
+            time.sleep(1)
+            insert = random.randint(1, len(reply))
+            reply = reply[:insert] + "&" + reply[insert:]
+        huaji = "[emotion pic_type=1 width=30 height=30]http://tb2.bdstatic.com/tb/editor/images/face/i_f25.png?t=20140803[/emotion]"
+        postData["content"] = reply.replace("&", huaji)
+        try:
+            print("回帖:" + postData["tid"])
+            resp = s.post(postUrl, postData)
+            print(resp.text)
+            success = True
+            time.sleep(random.randint(10, 25))
+        except Exception as e:
+            print(e)
 
 
 def PostReply():
     s = requests.Session()
+    #header
     s.headers = {
         "Host": "tieba.baidu.com",
         "Connection": "keep-alive",
         "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
     }
-
+    #读取fiddler抓取的cookie
     cookies = dict()
     cookieTxt = open("Data/CapturedData/tiebacok.txt", "r").read().replace("\t", "")
     for cok in re.findall(r"\S+=\n\S+\n\n", cookieTxt):
@@ -26,37 +48,54 @@ def PostReply():
             c = cok.split("=")
             cookies[c[0]] = c[1]
     s.cookies.update(cookies)
-
-    postUrl = "PostUrl"
+    #读取配置
+    config = dict()
+    for con in open("Data/tiebaconfig.txt", "r", encoding="utf-8").read().split("\n"):
+        if con != "":
+            i = con.index(":")
+            config[con[0:i]] = con[i + 1:]
+    t = config["TemplateNum"]
+    postUrl = config["PostUrl"]
+    #读取post值，request post时会自动编码，读取的值先解码
+    #设置为配置值，第一次访问成功可以保存返回cookie，下次读取使用(目前直接读取字典)
     postData = dict()
-    #request post时会自动编码，读取的值先解码
     for dat in urllib.parse.unquote(open("Data/CapturedData/tiebadat.txt", "r", encoding="utf-8").read()).split("&"):
         d = dat.split("=")
         postData[d[0]] = d[1]
-    #第一次访问成功可以保存返回cookie，下次读取使用(目前直接读取字典)
-    # postData["content"] = "开始"
-    # resp = s.post(postUrl, postData)
-    # print("第0次回复")
-    # print(resp.text)
-
-    reply1 = "半分钟又过去了，你还在浪费生命。"
-    reply2 = "一分钟又过去了，你还在浪费生命。"
-    for i in range(1, 101):
-        time.sleep(30)
-        if i % 2 == 0:
-            postData["content"] = reply2
-        else:
-            postData["content"] = reply1
-        success = False
-        while not success:
-            try:
-                resp = s.post(postUrl, postData)
-                print("第" + str(i) + "次回复")
-                print(resp.text)
-                i += 1
-                success = True
-            except Exception as e:
-                print(e)
+    postData["fid"] = config["fid" + t]
+    postData["kw"] = config["kw" + t]
+    if "tid" + t in config.keys():
+        postData["tid"] = config["tid" + t]
+    else:
+        postData["tid"] = ""
+    reply = config["content" + t]
+    #开始回复
+    # for i in range(1, 10):
+    #     PostContent(s, postUrl, reply, postData)
+    #     time.sleep(random.randint(20, 40))
+    tieSet = set()
+    first = 1
+    while True:
+        try:
+            resp = s.get(config["listUrl" + t])
+            print("第" + str(first) + "次刷新-------------")
+            time.sleep(15)
+            rt = 0
+            for tid in re.findall(r"href=\"/p/\d+", resp.text):
+                id = re.search(r"\d+", tid).group()
+                if first == 1:
+                    tieSet.add(id)
+                elif rt > 6:
+                    tieSet.add(id)
+                elif id not in tieSet:
+                    postData["tid"] = id
+                    PostContent(s, postUrl, reply, postData)
+                    tieSet.add(id)
+                    rt += 1
+            first += 1
+        except Exception as e:
+            print(e)
+            time.sleep(10)
 
 
 if __name__ == "__main__":
